ec4fca9bc4a9df07f27930e195efc4ddc2d57417
David Fosdick <david.fosdick@2132.io>
2021-05-27 11:08:03 +0100

    config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,14 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-const config: TypeOrmModuleOptions = {
+export default <TypeOrmModuleOptions>{
   type: 'mysql',
   host: 'localhost',
   port: 3306,
   username: 'root',
-  password: 'logitech',
+  password: 'admin',
   database: 'nestjs',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
-
-export default config
